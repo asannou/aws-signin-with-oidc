93e1ebe350e299f5baffe477e253144e2ccf0fe3
asannou <hataori@gmail.com>
2017-08-23 16:49:25 +0900

    remove reference to aws_s3_bucket.web

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_iam_openid_connect_provider" "google" {
 data "template_file" "origin" {
   template = "https://$${bucket}.s3$${region}.amazonaws.com"
   vars {
-    bucket = "${aws_s3_bucket.web.bucket}"
+    bucket = "${var.s3_bucket}"
     region = "${var.aws_region == "us-east-1" ? "" : "-${var.aws_region}"}"
   }
 }
